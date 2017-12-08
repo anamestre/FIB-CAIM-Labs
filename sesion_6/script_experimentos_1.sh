44c97900263305e3d7cba95bcaa5f20e7584946c
thisisana <ana.gabriela.mestre@est.fib.upc.edu>
2017-12-08 19:11:24 +0100

    Cleaning repo

--- sesion_6/script_experimentos_1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "ExtractData.py --index news --minfreq $1 --maxfreq $2 --numwords $3" > outputMRK.txt
-python ExtractData.py --index news --minfreq $1 --maxfreq $2 --numwords $3
-python GeneratePrototypes.py --nclust 20
-echo "MRK MEANS: " >> outputMRK.txt
-python MRKmeans.py >> outputMRK.txt 
-echo "PROCESS RESULTS: " >> outputMRK.txt
-python ProcessResults.py >> outputMRK.txt
